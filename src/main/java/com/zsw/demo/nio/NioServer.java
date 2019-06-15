af8ec01058b8495b3e4ee5dc10249bcbc01132bc
zhangshaowei <zhangshaowei>
2019-06-15 20:23:19 +0800

    update

--- src/main/java/com/zsw/demo/nio/NioServer.java
+++ src/main/java/com/zsw/demo/nio/NioServer.java
@@ -5,19 +5,17 @@ import lombok.SneakyThrows;
 import java.util.concurrent.TimeUnit;
 
 /**
- * @author Administrator on 2019/6/15 13:42
+ * @author Administrator on 2019/6/15 19:44
  **/
 public class NioServer {
 
     @SneakyThrows
     public static void main(String[] args) {
-        Thread t = new Thread(new ServerSocketChannelHandler(8088));
-
-        t.start();
+        Thread thread = new Thread(new ServerSocketChannelHandler(8088));
+        thread.start();
 
         TimeUnit.SECONDS.sleep(30);
-
-        t.interrupt();
+        thread.interrupt();
     }
 
 }
